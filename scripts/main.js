4af5fb862be81b5a62c7b5611043325988f4e6c1
aapxx87 <57785764+aapxx87@users.noreply.github.com>
2024-02-20 12:16:10 +0300

    new

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+const planets = document.querySelectorAll('.orbiting-circle');
+
+const orbitRadius = [50, 65, 80, 95, 120, 125, 140, 155];
+
+const speed = [0.01, 0.0075, 0.00625, 0.005, 0.00375, 0.0025, 0.002, 0.0015];
+
+const center = { x: 200, y: 200 };
+
+function animate() {
+
+  planets.forEach((planet, index) => {
+    const angle = speed[index] * Date.now() / 100;
+    const x = center.x + orbitRadius[index] * Math.cos(angle) - planet.offsetWidth / 2;
+    const y = center.y + orbitRadius[index] * Math.sin(angle) - planet.offsetHeight / 2;
+    planet.style.transform = `translate(${x}px, ${y}px)`;
+  });
+
+  requestAnimationFrame(animate);
+
+}
+
+animate();
