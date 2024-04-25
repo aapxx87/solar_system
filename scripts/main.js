d1973ccb7180107c4abb7618e546556a1bfc845f
aapxx87 <57785764+aapxx87@users.noreply.github.com>
2024-04-25 13:00:55 +0300

    update

--- scripts/main.js
+++ scripts/main.js
@@ -20,7 +20,7 @@ const create_html_sanbox_list = function () {
     <div class="planet-item_sandbox">
 
       <div class="planet_sandbox_img">
-        <img class="planet_list_img planet_list_img_sandbox" src="${url}" alt="">
+        <img class="planet_list_img planet_list_img_sandbox" src="${url}" alt="" id="planet_${idx}">
       </div>
 
       <div class="planet_sandbox_target_place">
@@ -43,8 +43,207 @@ create_html_sanbox_list()
 const planets_imgs = document.querySelectorAll('.planet_list_img_sandbox')
 const place_for_planets = document.querySelectorAll('.place_for_planet')
 
+
+const check_pair = function (arr) {
+
+  let id
+
+  if (arr.length === 2 && arr[0]) {
+
+    if (arr[0].includes('mercury') && arr[1] === '7') {
+      console.log('🎆 place 1 = Mercury');
+
+      document.getElementById(`${arr[1]}`).style.background = `url('${arr[0]}')`
+      document.getElementById(`${arr[1]}`).style.backgroundSize = 'cover'
+      document.getElementById(`${arr[1]}`).style.backgroundPosition = 'bottom'
+
+
+
+      planets_imgs.forEach(function (item) {
+
+        if (item.src.includes(`${arr[0]}`)) {
+          console.log(item.id);
+          id = item.id
+        }
+
+
+      })
+
+      document.getElementById(`${id}`).remove()
+
+      id = ''
+
+
+      match_answer_arr = []
+    } else if (arr[0].includes('venus') && arr[1] === '6') {
+      console.log('🎆  place 2 = Venus');
+
+      document.getElementById(`${arr[1]}`).style.background = `url('${arr[0]}')`
+      document.getElementById(`${arr[1]}`).style.backgroundSize = 'cover'
+      document.getElementById(`${arr[1]}`).style.backgroundPosition = 'bottom'
+
+      planets_imgs.forEach(function (item) {
+
+        if (item.src.includes(`${arr[0]}`)) {
+          console.log(item.id);
+          id = item.id
+        }
+
+
+      })
+
+      document.getElementById(`${id}`).remove()
+
+      id = ''
+
+      match_answer_arr = []
+    } else if (arr[0].includes('earth') && arr[1] === '5') {
+      console.log('🎆  place 3 = Earth');
+
+      document.getElementById(`${arr[1]}`).style.background = `url('${arr[0]}')`
+      document.getElementById(`${arr[1]}`).style.backgroundSize = 'cover'
+      document.getElementById(`${arr[1]}`).style.backgroundPosition = 'bottom'
+
+      planets_imgs.forEach(function (item) {
+
+        if (item.src.includes(`${arr[0]}`)) {
+          console.log(item.id);
+          id = item.id
+        }
+
+
+      })
+
+      document.getElementById(`${id}`).remove()
+
+      id = ''
+
+      match_answer_arr = []
+    } else if (arr[0].includes('mars') && arr[1] === '4') {
+      console.log('🎆  place 4 = Mars');
+
+      document.getElementById(`${arr[1]}`).style.background = `url('${arr[0]}')`
+      document.getElementById(`${arr[1]}`).style.backgroundSize = 'cover'
+      document.getElementById(`${arr[1]}`).style.backgroundPosition = 'bottom'
+
+      planets_imgs.forEach(function (item) {
+
+        if (item.src.includes(`${arr[0]}`)) {
+          console.log(item.id);
+          id = item.id
+        }
+
+
+      })
+
+      document.getElementById(`${id}`).remove()
+
+      id = ''
+
+      match_answer_arr = []
+    } else if (arr[0].includes('jupiter') && arr[1] === '3') {
+      console.log('🎆  place 5 = Jupiter');
+
+      document.getElementById(`${arr[1]}`).style.background = `url('${arr[0]}')`
+      document.getElementById(`${arr[1]}`).style.backgroundSize = 'cover'
+      document.getElementById(`${arr[1]}`).style.backgroundPosition = 'bottom'
+
+      planets_imgs.forEach(function (item) {
+
+        if (item.src.includes(`${arr[0]}`)) {
+          console.log(item.id);
+          id = item.id
+        }
+
+
+      })
+
+      document.getElementById(`${id}`).remove()
+
+      id = ''
+
+      match_answer_arr = []
+    } else if (arr[0].includes('saturn') && arr[1] === '2') {
+      console.log('🎆  place 6 = saturn');
+
+      document.getElementById(`${arr[1]}`).style.background = `url('${arr[0]}')`
+      document.getElementById(`${arr[1]}`).style.backgroundSize = 'cover'
+      document.getElementById(`${arr[1]}`).style.backgroundPosition = 'bottom'
+
+      planets_imgs.forEach(function (item) {
+
+        if (item.src.includes(`${arr[0]}`)) {
+          console.log(item.id);
+          id = item.id
+        }
+
+
+      })
+
+      document.getElementById(`${id}`).remove()
+
+      id = ''
+
+      match_answer_arr = []
+    } else if (arr[0].includes('uranus') && arr[1] === '1') {
+      console.log('🎆  place 7 = Uranus');
+
+      document.getElementById(`${arr[1]}`).style.background = `url('${arr[0]}')`
+      document.getElementById(`${arr[1]}`).style.backgroundSize = 'cover'
+      document.getElementById(`${arr[1]}`).style.backgroundPosition = 'bottom'
+
+      planets_imgs.forEach(function (item) {
+
+        if (item.src.includes(`${arr[0]}`)) {
+          console.log(item.id);
+          id = item.id
+        }
+
+
+      })
+
+      document.getElementById(`${id}`).remove()
+
+      id = ''
+
+      match_answer_arr = []
+    } else if (arr[0].includes('neptune') && arr[1] === '0') {
+      console.log('🎆  place 8 = Neptune');
+
+      document.getElementById(`${arr[1]}`).style.background = `url('${arr[0]}')`
+      document.getElementById(`${arr[1]}`).style.backgroundSize = 'cover'
+      document.getElementById(`${arr[1]}`).style.backgroundPosition = 'bottom'
+
+      planets_imgs.forEach(function (item) {
+
+        if (item.src.includes(`${arr[0]}`)) {
+          console.log(item.id);
+          id = item.id
+        }
+
+
+      })
+
+      document.getElementById(`${id}`).remove()
+
+      id = ''
+
+      match_answer_arr = []
+    } else {
+      console.log('❌ wrong answer');
+      match_answer_arr = []
+    }
+
+  }
+
+}
+
+
+
 let img
 let planet_place
+
+let match_answer_arr = []
 
 
 const find_clicked_img = function () {
@@ -52,7 +251,13 @@ const find_clicked_img = function () {
   planets_imgs.forEach(function (item) {
     item.addEventListener('click', function () {
       let img = item.src
-      console.log(img);
+
+      match_answer_arr[0] = img
+
+      // console.log(img);
+      // console.log(match_answer_arr);
+
+      check_pair(match_answer_arr)
     })
   })
 
@@ -64,20 +269,45 @@ find_clicked_img()
 
 
 
+const highlight_target_place = function (id) {
+
+  place_for_planets.forEach(function (item) {
+    item.classList.remove('border')
+  })
+
+  document.getElementById(`${id}`).classList.add('border')
+
+}
+
+
+
+
+
 const find_planet_place = function () {
 
   place_for_planets.forEach(function (item) {
     item.addEventListener('click', function () {
       let planet_place = item.id
-      console.log(planet_place);
+
+      match_answer_arr[1] = planet_place
+      // console.log(planet_place);
+      // console.log(match_answer_arr);
+      check_pair(match_answer_arr)
+      highlight_target_place(planet_place)
     })
   })
 
 
 }
 
-
 find_planet_place()
+
+
+
+
+
+
+
 
 
 
